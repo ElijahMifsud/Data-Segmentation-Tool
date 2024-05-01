b77238123f809c30b8d20e30495815625f3be572
Kane-Heaver <73532960+Kane-Heaver@users.noreply.github.com>
2024-05-01 11:13:46 +0930

    Add ability to select multiple segments, save multiple csv files and fixed up scaling issue/ comment code

--- Data Segmentation Tool.py
+++ Data Segmentation Tool.py
@@ -14,8 +14,7 @@ class Main(QMainWindow):
         self.setWindowTitle("Data Segmentation Tool")
         self.centralwidget = QWidget()
         self.checkboxList = []
-
-
+        self.dataFrame_List = []
 
         font = QtGui.QFont()
         font.setPointSize(20)
@@ -36,8 +35,6 @@ class Main(QMainWindow):
         font.setPointSize(10)
         self.uploadButton.setFont(font)
         self.uploadButton.clicked.connect(self.importCSV)
-        ##self.uploadButton.clicked.connect(self.create_checkboxes(font)) 
-
 
         # fileName
         self.fileName = QTextEdit(self.centralwidget)
@@ -46,7 +43,7 @@ class Main(QMainWindow):
         # SegmantLabel   
         self.segmantLabel = QLabel(self.centralwidget)
         self.segmantLabel.setGeometry(QtCore.QRect(100, 260, 100, 31))
-        self.segmantLabel.setText("Segmant:")
+        self.segmantLabel.setText("Segment:")
         font.setPointSize(14)
         self.segmantLabel.setFont(font)
         self.segmantLabel.setAlignment(QtCore.Qt.AlignCenter)
@@ -93,30 +90,11 @@ class Main(QMainWindow):
         self.verticalLayout = QVBoxLayout(self.widget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
 
-        font.setPointSize(12)
-        font.setBold(False)
-        font.setWeight(50)        
-
-
-        # self.segmant1 = QCheckBox(self.widget)
-        # self.segmant1.setText("   1")
-        # self.segmant1.setFont(font)
-        # self.verticalLayout.addWidget(self.segmant1)
-
-        # self.segmant2 = QCheckBox(self.widget)
-        # self.segmant2.setText("   2")
-        # self.segmant2.setFont(font)
-        # self.verticalLayout.addWidget(self.segmant2)
-
-        # self.segmant3 = QCheckBox(self.widget)
-        # self.segmant3.setText("   3")
-        # self.segmant3.setFont(font)
-        # self.verticalLayout.addWidget(self.segmant3)
-
         self.setCentralWidget(self.centralwidget) 
 
 
     def importCSV(self):
+        # Picks the file to be imported
         fname = QFileDialog.getOpenFileName(self, 'Open file', 
             'c:\\',"CSV files (*.csv)")
         if fname[0]:
@@ -125,82 +103,107 @@ class Main(QMainWindow):
             self.create_checkboxes()
 
     def submitButtonClicked(self):
-
+        self.segment_List = []
+        # Checks which checkboxes are checked and adds them to a list
         for box in self.checkboxList:
             if box.isChecked():
                 segment = int(box.text())
+                self.segment_List.append(segment)
 
-        time_range = self.timeRangeInput.toPlainText().split(',')
+        # Loops over list of checked checkboxes to segment
+        for segment in self.segment_List:
+            time_range = self.timeRangeInput.toPlainText().split(',')
 
-        # Converts Timestamp to a datetime so we can use within dataframes
-        self.df['Timestamp'] = pd.to_datetime(self.df['Timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
+            # Converts Timestamp to a datetime so we can use within dataframes
+            self.df['Timestamp'] = pd.to_datetime(self.df['Timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
 
-        segment_df = self.df[self.df['Segment'] == segment]
-        min_time = segment_df['Timestamp'].min()
+            segment_df = self.df[self.df['Segment'] == segment]
+            min_time = segment_df['Timestamp'].min()
 
-        #Validate user input
-        smallest_time = self.df['Timestamp'].min()
-        maximum_time = self.df['Timestamp'].max()
+            # Validate user input
+            smallest_time = self.df['Timestamp'].min()
+            maximum_time = self.df['Timestamp'].max()
 
-        # Check if time_range contains exactly two elements    
-        if len(time_range) != 2:
-            self.buttonConfirmation("Invalid Input two integers are needed. Format: -x,x")
-            return
+            # Check if time_range contains exactly two elements    
+            if len(time_range) != 2:
+                self.buttonConfirmation("Invalid Input two integers are needed. Format: -x,x")
+                return
 
-        # Check if both elements in time_range are integers
-        try:
-            time_range = [int(i) for i in time_range]
-        except ValueError:
-            self.buttonConfirmation("Invalid Input two integers are needed. Format: -x,x ")
-            return     
+            # Check if both elements in time_range are integers
+            try:
+                time_range = [int(i) for i in time_range]
+            except ValueError:
+                self.buttonConfirmation("Invalid Input Two Integers are needed. Format: -x,x ")
+                return     
 
-        if min_time + pd.Timedelta(seconds=time_range[0]) < smallest_time or min_time + pd.Timedelta(seconds=time_range[1]) > maximum_time:
-            self.buttonConfirmation("Invalid min or max time. Format: -x,x")
-        else:
-            start_time = min_time + pd.Timedelta(seconds=time_range[0])
-            end_time = min_time + pd.Timedelta(seconds=time_range[1])
+            if min_time + pd.Timedelta(seconds=time_range[0]) < smallest_time or min_time + pd.Timedelta(seconds=time_range[1]) > maximum_time:
+                self.buttonConfirmation("Invalid Min or Max time. Format: -x,x")
+                return
+            
+            else:
+                # Converts the time_range user input
+                start_time = min_time + pd.Timedelta(seconds=time_range[0])
+                end_time = min_time + pd.Timedelta(seconds=time_range[1])
 
-            self.new_df = self.df[(self.df['Timestamp'] >= start_time) & 
-                                (self.df['Timestamp'] <= end_time)]
+                # Makes a new dataframe from the start and end times selected and adds to a dataframe list
+                new_df = self.df[(self.df['Timestamp'] >= start_time) & 
+                                    (self.df['Timestamp'] <= end_time)]
+                
+                self.dataFrame_List.append(new_df)
 
-            self.buttonConfirmation("Submition Successful, Click the Download Button to Save the CSV")
+        self.buttonConfirmation("Submission Successful, Click the Download Button to Save the CSV File/s")
 
     def exportCSV(self):
-        fname = QFileDialog.getSaveFileName(self, 'Save file', 
-            'c:\\',"CSV files (*.csv)")
-        if fname[0]:
-            self.new_df.to_csv(fname[0], index=False)
+        # Saves csv files to a selected location
+        for dataframe in self.dataFrame_List:
+            fname = QFileDialog.getSaveFileName(self, 'Save file', 
+                'c:\\',"CSV files (*.csv)")
+            if fname[0]:
+                dataframe.to_csv(fname[0], index=False)
     
     def buttonConfirmation(self, string):
+        # Creates a button confirmation
         msgbox = QMessageBox()
         msgbox.setWindowTitle("Confirmation")
         msgbox.setText(string)
         msgbox.exec()
 
     def create_checkboxes(self):
+        self.clear_checkboxes()
+
         font = QtGui.QFont()
         font.setPointSize(15)
         font.setBold(False)
-        font.setWeight(30) 
+        font.setWeight(20) 
 
         # Get the unique segments from the 'Segment' column
         segments = self.df['Segment'].unique()
-
         
         # Create a checkbox for each segment
         for i, segment in enumerate(segments, start=1):
+            # Create check boxes
             checkbox = QCheckBox(self.widget)
-            checkbox.setText(f"   {segment}")  # Use the segment value as the checkbox text
-            checkbox.setStyleSheet("QCheckBox::indicator { width: 30px; height: 30px;}")
+            # Use the segment value as the checkbox text
+            checkbox.setText(f"   {segment}")  
             checkbox.setFont(font)
-
-            self.widget.setGeometry(QtCore.QRect(100, 300,81, 300))
+            # Adds correct spacing to larger number of segments
+            if segments.size > 4:
+                self.widget.setGeometry(QtCore.QRect(100, 300,81, 300))
+            elif  segments.size <= 4:
+                self.widget.setGeometry(QtCore.QRect(100, 300,81, 100))
             self.verticalLayout.addWidget(checkbox)
             self.checkboxList.append(checkbox)
             
         self.setCentralWidget(self.centralwidget)
         
+    def clear_checkboxes(self):
+        # Clear the list of checkboxes
+        for checkbox in self.checkboxList:
+            self.verticalLayout.removeWidget(checkbox)
+            checkbox.deleteLater()  
+        self.checkboxList.clear()  
 
+        
 app = QApplication(sys.argv)
 window = Main()
 window.show()
